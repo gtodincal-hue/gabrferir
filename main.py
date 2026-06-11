7d0d7a6b7bc227d6cd4755855bb4354774bad5ae
Gabriel Lopes <gabriel@gmail.com>
2026-06-11 00:45:33 -0300

    Add main loop

---
+++ main.py
@@ -0,0 +1,11 @@
+continuar = True
+while continuar is True:
+    print("Deseja continuar? (s/n)")
+    resposta = input()
+    if resposta.lower() == "s":
+        print("Continuando...")
+    elif resposta.lower() == "n":
+        print("Encerrando...")
+        continuar = False
+    else:
+        print("Resposta inválida. Por favor, digite 's' para sim ou 'n' para não.")
