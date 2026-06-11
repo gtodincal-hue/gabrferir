36ae96b9eed9e522f40a8e732806c9f334696640
Gabriel Lopes <gabriel@gmail.com>
2026-06-11 00:54:09 -0300

    contador

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-continuar = True
-while continuar is True:
-    print("Deseja continuar? (s/n)")
-    resposta = input()
-    if resposta.lower() == "s":
-        print("Continuando...")
-    elif resposta.lower() == "n":
-        print("Encerrando...")
-        continuar = False
-    else:
-        print("Resposta inválida. Por favor, digite 's' para sim ou 'n' para não.")
-    print("Mamamamammama")
+contador=0
+while contador<20:
+    print(contador)
+    contador+=1
